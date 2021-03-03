d6fa7a89f828f3a20b7a3d8a73cab5a73726d3ec
pksatyam777 <pksatyam777@gmail.com>
2021-03-03 16:25:26 +0530

    Updates

---
+++ precache-manifest.2b1ab3e8852d52cf3ee5c2f0b5270ab3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab8d75aef6b7e14db94e27bc8fb5ebc",
+    "url": "/Todo-Redux/index.html"
+  },
+  {
+    "revision": "7d2bde1aa32c2cd1ef2f",
+    "url": "/Todo-Redux/static/css/2.79348e54.chunk.css"
+  },
+  {
+    "revision": "97edfe3441cb016ff183",
+    "url": "/Todo-Redux/static/css/main.a6abe02e.chunk.css"
+  },
+  {
+    "revision": "7d2bde1aa32c2cd1ef2f",
+    "url": "/Todo-Redux/static/js/2.868dfbf6.chunk.js"
+  },
+  {
+    "revision": "28f29a598e465553cfeea810767399d3",
+    "url": "/Todo-Redux/static/js/2.868dfbf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97edfe3441cb016ff183",
+    "url": "/Todo-Redux/static/js/main.fbe3298d.chunk.js"
+  },
+  {
+    "revision": "b252afa19e109bff9e30",
+    "url": "/Todo-Redux/static/js/runtime-main.50c72b09.js"
+  }
+]);
